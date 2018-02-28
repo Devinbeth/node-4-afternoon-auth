e169cd882321d36958ddbedfffb9595d86f055ca
Devin Lethbridge <devinbeth@gmail.com>
2018-02-28 15:44:02 -0700

    more things

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ passport.use(new Auth0Strategy({
     domain: DOMAIN,
     clientID: CLIENT_ID,
     clientSecret: CLIENT_SECRET,
-    callbackURL: '/login',
+    callbackURL: '/auth/callback',
     scope: "openid email profile"
 },
     function (accessToken, refreshToken, extraParams, profile, done) {
@@ -35,16 +35,17 @@ passport.deserializeUser((obj, done) => {
     done(null, obj);
 });
 
-app.get('/login',
-    passport.authenticate('auth0',
-        { successRedirect: '/students', failureRedirect: '/login', connection: 'github' }
-    )
-);
+app.get(`/auth/`, passport.authenticate(`auth0`));
+app.get(`/auth/callback`, passport.authenticate(`auth0`, {
+    successRedirect: '/students',
+    failureRedirect: '/login'
+}));
 
 function authenticated(req, res, next) {
     if (req.user) {
         next()
-    } else {
+    }
+    else {
         res.sendStatus(401);
     }
 }
